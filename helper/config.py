45bebe2081993d327e5796770cb6627f573f5391
Official Bika <phoetaay@gmail.com>
2026-07-22 13:33:16 +0700

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -5,3 +5,25 @@ API_HASH = os.getenv("API_HASH", "")
 SESSION_STRING = os.getenv("SESSION_STRING", "")
 
 TARGET_CHAT = os.getenv("HELPER_TARGET_CHAT", "")
+
+SOURCES = {
+    "catchbot": {
+        "command": "/check",
+    },
+
+    "graden": {
+        "command": "/check",
+    },
+
+    "senpai": {
+        "command": "/see",
+    },
+
+    "hallow": {
+        "command": "/show",
+    },
+
+    "takers": {
+        "command": "/detect",
+    }
+}
