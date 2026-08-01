0f1f145ee3f8aa98b00deef2d8d14f600d4d4dab
Official Bika <phoetaay@gmail.com>
2026-08-02 01:17:10 +0700

    V3: add async task service layer

---
+++ app/services/task_service.py
@@ -0,0 +1,15 @@
+import asyncio
+
+
+class TaskService:
+    def __init__(self):
+        self.tasks = set()
+
+    def create(self, coroutine):
+        task = asyncio.create_task(coroutine)
+        self.tasks.add(task)
+        task.add_done_callback(self.tasks.discard)
+        return task
+
+
+ task_service = TaskService()
