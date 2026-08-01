8ce74c3db91f9a4e2677b5a06b8ff0f235f1aaae
Official Bika <phoetaay@gmail.com>
2026-08-02 01:22:39 +0700

    V3: add database index definitions

---
+++ app/database/indexes.py
@@ -0,0 +1,13 @@
+IMAGE_INDEXES = [
+    {
+        "keys": [("hash_value", 1)],
+        "unique": False,
+    }
+]
+
+USER_INDEXES = [
+    {
+        "keys": [("user_id", 1)],
+        "unique": True,
+    }
+]
