1148373c0b6baae1c5b2954b9bbfca1536650ab1
Official Bika <phoetaay@gmail.com>
2026-08-02 01:20:35 +0700

    V3: integrate user registration into start handler

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -2,9 +2,12 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from app.services.user_service import user_service
+
 router = Router()
 
 
 @router.message(CommandStart())
 async def start_handler(message: Message):
+    await user_service.register(message.from_user)
     await message.answer("Adding HelperBot V3 is running")
