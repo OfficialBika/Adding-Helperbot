f5472d6ac5b88cf0cedab3ebb3117ba1133738ea
Official Bika <phoetaay@gmail.com>
2026-08-02 01:18:39 +0700

    V3: register image handler

--- app/handlers/loader.py
+++ app/handlers/loader.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher
 
 from app.handlers.start import router as start_router
+from app.handlers.image_handler import router as image_router
 
 
 def register_handlers(dp: Dispatcher):
     dp.include_router(start_router)
+    dp.include_router(image_router)
