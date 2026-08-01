e553ad333fcccc943749646b0f6b175b4e193029
Official Bika <phoetaay@gmail.com>
2026-08-02 01:20:47 +0700

    V3: add admin and pyrogram settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,8 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
     BOT_TOKEN = os.getenv("BOT_TOKEN", "")
     DATABASE_URL = os.getenv("DATABASE_URL", "")
+    API_ID = os.getenv("API_ID", "")
+    API_HASH = os.getenv("API_HASH", "")
+    ADMIN_IDS = os.getenv("ADMIN_IDS", "")
+
 
 settings = Settings()
