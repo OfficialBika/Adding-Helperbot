c4ba3b11aa4137339d2fa7992f0dc90982338853
Official Bika <phoetaay@gmail.com>
2026-08-02 09:48:01 +0700

    Expose database lifecycle helpers

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,11 @@
 from .client import database
 from .repositories import items_repo, users_repo
+from .lifecycle import startup_database, shutdown_database
 
-__all__ = ['database', 'items_repo', 'users_repo']
+__all__ = [
+    'database',
+    'items_repo',
+    'users_repo',
+    'startup_database',
+    'shutdown_database',
+]
