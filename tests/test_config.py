2b5bc0ed49032e0327c7a65aa3fcec906a4385c2
Official Bika <phoetaay@gmail.com>
2026-08-02 09:17:23 +0700

    Delete tests directory

--- tests/test_config.py
+++
@@ -1,6 +0,0 @@
-from app.config.validator import config_validator
-
-
-def test_config_validator():
-    result = config_validator.validate()
-    assert isinstance(result, list)
--- tests/test_services.py
+++
@@ -1,8 +0,0 @@
-from app.services.statistics_service import statistics_service
-
-
-def test_statistics_service():
-    statistics_service.add_image()
-    data = statistics_service.get()
-
-    assert data["images"] >= 1
